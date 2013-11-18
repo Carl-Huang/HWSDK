6c9d1c02e59838f2a6bcdce409a82d28d4654db3
Carl_Huang <Carl_Huang@Carl-HuangtekiMac-mini.local>
2013-11-18 15:00:39 +0800

    Initial Commit

---
+++ HWSDK/HWSDK.h
@@ -0,0 +1,13 @@
+//
+//  HWSDK.h
+//  HWSDK
+//
+//  Created by Carl_Huang on 13-11-18.
+//  Copyright (c) 2013年 HelloWorld. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HWSDK : NSObject
+
+@end
---
+++ HWSDK/HWSDK.m
@@ -0,0 +1,13 @@
+//
+//  HWSDK.m
+//  HWSDK
+//
+//  Created by Carl_Huang on 13-11-18.
+//  Copyright (c) 2013年 HelloWorld. All rights reserved.
+//
+
+#import "HWSDK.h"
+
+@implementation HWSDK
+
+@end
